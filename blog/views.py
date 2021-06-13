2632a1bc08599bf952af914da5e564ffac11edc4
debojyotichatterjee <debojyoti.chatterjee9@gmail.com>
2021-06-13 18:03:18 +0530

    landing page skel

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from django.http.response import Http404
 
 
 # Create your views here.
 
 # landing page
 def home(request):
-    return HttpResponse("Home Page!!!")
+    try:
+        # this is a shortcut method
+        return render(request, "blog/index.html")
+    except:
+        raise Http404()
 
 # post list
+
+
 def posts(request):
     return HttpResponse("Post List!!!")
 
 # post detail
+
+
 def post_detail(request):
     return HttpResponse("Post Detail!!!")
