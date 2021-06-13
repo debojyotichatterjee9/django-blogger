7f07fa6797effc7f549bd0a32e0a3dc3c182fbc4
debojyotichatterjee <debojyoti.chatterjee9@gmail.com>
2021-06-13 22:58:12 +0530

    working on templates

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,85 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, HttpResponseNotFound
 from django.http.response import Http404
-
+from datetime import date
 
 # Create your views here.
 
-all_posts = {
-    
-}
+all_posts = [
+    {
+        "slug": "hike-mountains",
+        "image": "mountains.jpg",
+        "author": "Fred Barner",
+        "date": date(2021, 7, 20),
+        "title": "Mountain Hiking",
+        "excerpt": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex labore voluptate...",
+        "content": """
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Minus reiciendis veniam inventore eligendi ad corporis unde a 
+        recusandae nostrum quos aliquam ullam, cum repellendus doloremque 
+        fuga voluptas suscipit velit iste.
+        """
+
+    },
+    {
+        "slug": "new-laptop",
+        "image": "laptop.jpg",
+        "author": "Fred Barner",
+        "date": date(2021, 7, 5),
+        "title": "New laptop",
+        "excerpt": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex labore voluptate...",
+        "content": """
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Minus reiciendis veniam inventore eligendi ad corporis unde a 
+        recusandae nostrum quos aliquam ullam, cum repellendus doloremque 
+        fuga voluptas suscipit velit iste.
+        """
+
+    },
+    {
+        "slug": "forest-tour",
+        "image": "forest.jpg",
+        "author": "Fred Barner",
+        "date": date(2021, 12, 20),
+        "title": "Forest Tour",
+        "excerpt": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex labore voluptate...",
+        "content": """
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Minus reiciendis veniam inventore eligendi ad corporis unde a 
+        recusandae nostrum quos aliquam ullam, cum repellendus doloremque 
+        fuga voluptas suscipit velit iste.
+        """
+
+    },
+    {
+        "slug": "tech-stuff",
+        "image": "laptop.jpg",
+        "author": "Fred Barner",
+        "date": date(2021, 3, 15),
+        "title": "Tech Stuff",
+        "excerpt": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex labore voluptate...",
+        "content": """
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Minus reiciendis veniam inventore eligendi ad corporis unde a 
+        recusandae nostrum quos aliquam ullam, cum repellendus doloremque 
+        fuga voluptas suscipit velit iste.
+        """
+
+    }
+]
+def get_date(post):
+    return post['date']
 # landing page
 def home(request):
+    sorted_post = sorted(all_posts, key=get_date)
+    latest_posts = sorted_post[-2:]
     try:
         # this is a shortcut method
-        return render(request, "blog/index.html")
-    except:
-        raise Http404()
+        return render(request, "blog/index.html", {
+            "posts": latest_posts
+        })
+    except Exception as e:
+        return HttpResponseNotFound(e)
 
 # post list
 
