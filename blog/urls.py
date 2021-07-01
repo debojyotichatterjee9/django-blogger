7ba337eca7f8961443189cce3256ea0d3de843a9
debojyotichatterjee <debojyoti.chatterjee9@gmail.com>
2021-07-01 23:24:53 +0530

    added bookmark feature

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.HomePageView.as_view(), name="blog-home"),
     path("posts", views.AllPostsView.as_view(), name="blog-list"),
-    path("post/<slug:slug>", views.DetailPostView.as_view(), name="blog-detail")
+    path("post/<slug:slug>", views.DetailPostView.as_view(), name="blog-detail"),
+    path("read-later", views.ReadLaterView.as_view(), name="read-later")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.views import View
 from django.views.generic import ListView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
@@ -38,20 +38,29 @@ class AllPostsView(ListView):
 # post detail
 class DetailPostView(View):
 
+    def is_stored_post(self, request, post_id):
+        is_saved_for_later = False
+        stored_posts = request.session.get("stored_posts")
+        if stored_posts is not None or len(stored_posts) > 0:
+            is_saved_for_later = post_id in stored_posts
+        return is_saved_for_later
+
     def get(self, request, slug):
         post = Post.objects.get(slug=slug)
+        is_saved_for_later = self.is_stored_post(request, post.id)
         context = {
             "post": post,
             "post_tags": post.tags.all(),
             "comment_form": CommentForm(),
-            "comments": post.comments.all().order_by("-date")
+            "comments": post.comments.all().order_by("-date"),
+            "is_saved_for_later": is_saved_for_later
         }
         return render(request, "blog/post-detail.html", context)
 
     def post(self, request, slug):
         comment_form = CommentForm(request.POST)
         post = Post.objects.get(slug=slug)
-        
+        is_saved_for_later = self.is_stored_post(request, post.id)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False) # does not hit the db but creates an instance
             comment.post = post
@@ -62,6 +71,41 @@ class DetailPostView(View):
             "post": post,
             "post_tags": post.tags.all(),
             "comment_form": comment_form,
-            "comments": post.comments.all().order_by("-date")
+            "comments": post.comments.all().order_by("-date"),
+            "is_saved_for_later": is_saved_for_later
         }
         return render(request, "blog/post-detail.html", context)
+
+
+
+class ReadLaterView(View):
+
+    def get(self, request):
+        stored_posts = request.session.get("stored_posts")
+        context = {}
+
+        if stored_posts is None or len(stored_posts) == 0:
+            context["posts"] = []
+            context["posts_saved"] = False
+        else:
+            posts = Post.objects.filter(id__in=stored_posts)
+            context["posts"] = posts
+            context["posts_saved"] = True
+        print(context)
+        return render(request, "blog/stored-posts.html", context)
+
+    def post(self, request):
+        stored_posts = request.session.get("stored_posts")
+
+        if stored_posts is None:
+            stored_posts = []
+        post_id = int(request.POST["post_id"])
+        post_slug = request.POST["post_slug"]
+        if post_id not in stored_posts:
+            stored_posts.append(post_id)
+            request.session["stored_posts"] = stored_posts
+        else:
+            stored_posts.remove(post_id)
+            request.session["stored_posts"] = stored_posts
+        
+        return redirect("blog-detail", post_slug)
